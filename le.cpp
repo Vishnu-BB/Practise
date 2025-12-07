c15efff0c8d1a3881a6b352e0d0612a6554f2819
Vishnu-BB <kurravishnu.blubridge@evoplus.in>
2025-12-07 12:12:11 +0530

    buildArray

--- le.cpp
+++ le.cpp
@@ -58,24 +58,53 @@ using namespace std;
 
 // using std::string;
 
-class Solution {
-public:
-    int idx=0;
-    int strStr(string haystack, string needle) {
-        for(int j = 0; j < needle.length(); j++){
-            for(int i = 0; i < haystack.length(); i++){
-                if(haystack[i]==needle[j]){
-                    std::cout<<i;
-                }
-            }
-        }
-        return -1;
-    }
+// class Solution {
+// public:
+//     int idx=0;
+//     int strStr(string haystack, string needle) {
+//         for(int j = 0; j < needle.length(); j++){
+//             for(int i = 0; i < haystack.length(); i++){
+//                 if(haystack[i]==needle[j]){
+//                     std::cout<<i;
+//                 }
+//             }
+//         }
+//         return -1;
+//     }
  
-};
+// };
+
+// #include <algorithm>
+
+// class Solution {
+// public:
+//     vector<string> buildArray(vector<int>& target, int n) {
+//         vector<string> p;
+//         int c = 0;
+//         for(int i : target){
+//             c = max(i, c);
+//         }
+//         sort(target.begin(), target.end());
+//         for(int i = 0; i < c; i++){
+//             p.push_back("push");
+//             if((i+1) != target[i]){
+//                 cout<<i+1<< "  " <<target[i]<<'\n';
+//                 target.insert(target.begin()+i, target[i]);
+//                 p.push_back("pop");
+//             }
+//         }
+//         return p;
+//     }
+// };
+
 
 int main(){
     Solution s;
-    s.strStr("butsaid", "sad");
+    vector<int> target = {2, 3, 4};
+    int c = 4;
+    vector<string> ss = s.buildArray(target, c);
+    for(string i : ss){
+        cout<<i<<'\n';
+    }
     return 0;
 }
